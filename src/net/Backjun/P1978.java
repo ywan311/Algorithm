9efd2625b9624903cda5b97e3018dca564d95faa
yoowankim <ywan311@naver.com>
2021-01-10 21:55:15 +0900

    20210110

---
+++ src/net/Backjun/P1978.java
@@ -0,0 +1,27 @@
+package net.Backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1978 {
+    public static void main(String[] args) throws IOException {
+        boolean[] arr = new boolean[1001];
+        for(int i=2;i<500;i++){
+            for(int j = i+i;j<arr.length;j+=i ){
+                arr[j] =true;
+            }
+        }
+
+        int answer = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+
+        for(String s: input){
+            int value = Integer.parseInt(s);
+            if(value>1&&!arr[value])answer++;
+        }
+        System.out.println(answer);
+    }
+}
