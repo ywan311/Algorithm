078eb58d82d7534371d0e3b9a05a5a48e503cf12
yoowankim <ywan311@naver.com>
2021-01-12 20:36:42 +0900

    2020-01-12

---
+++ src/net/Backjun/P1100.java
@@ -0,0 +1,27 @@
+package net.Backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1100 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[][] arr = new boolean[8][8];
+
+        for(int i=0;i<8;i++){
+            String input = br.readLine();
+            for(int j=0;j<8;j++){
+                arr[i][j]=input.charAt(j)=='F';
+            }
+        }
+        int answer=0;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                if((i+j)%2==0&&arr[i][j])answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
