ab0cc1a923f2ba6ecf15d0a05161b1b4a0d0a176
yoowankim <ywan311@naver.com>
2021-01-14 18:09:12 +0900

    2020-01-14

--- src/net/Backjun/P1032.java
+++ src/net/Backjun/P1032.java
@@ -11,23 +11,27 @@ public class P1032 {
         int N = Integer.parseInt(br.readLine());
         String[] arr = new String[N];
 
-        for(int i=0;i<N;i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = br.readLine();
         }
-        if(N==1){
-            System.out.println(1);
+        if (N == 1) {
+            System.out.println(arr[0]);
             return;
         }
-        int length=0;
-        for(int i=0;i<arr[0].length();i++){
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < arr[0].length(); i++) {
             char c = arr[0].charAt(i);
-            boolean flag =false;
-            for(int j=1;j<N;j++){
-                if(arr[j].charAt(i)!=c){
-                    flag=true;
+            boolean flag = false;
+            for (int j = 1; j < N; j++) {
+                if (arr[j].charAt(i) != c) {
+                    flag = true;
                     break;
                 }
             }
+            if (flag) {
+                buffer.append('?');
+            } else buffer.append(c);
         }
+        System.out.println(buffer.toString());
     }
 }
