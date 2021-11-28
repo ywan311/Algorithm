085382115ba68dcf7e77f5eeeed8cc61f4594aae
yoowankim <ywan311@naver.com>
2021-11-28 16:58:23 +0900

    2021-11-28 효율성 통과

--- src/net/Programmers/ScoreSearch.java
+++ src/net/Programmers/ScoreSearch.java
@@ -3,56 +3,58 @@ package net.Programmers;
 import java.util.*;
 
 //이분탐색 필요 !!!!!
-//효율성 통과 못함
 public class ScoreSearch {
     static Map<String, ArrayList<Integer>> map = new HashMap<>();
+
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
 
-        for(String s : info){
+        for (String s : info) {
             String[] arr = s.split(" ");
-            getCombination(0,"", arr);
+            getCombination(0, "", arr);
+        }
+        List<String> list = new ArrayList<>(map.keySet());
+        for (int i = 0; i < list.size(); i++) {
+            List<Integer> scoreList = map.get(list.get(i));
+            Collections.sort(scoreList);
         }
 
-        for(int i =0;i<query.length;i++){
+        for (int i = 0; i < query.length; i++) {
             String key = query[i].replaceAll(" and ", "");
             String[] arr = key.split(" ");
-            if(!map.containsKey(arr[0])){
-                answer[i] = 0;
-            }else{
-                Collections.sort(map.get(arr[0]));
-                answer[i]=search(arr[0],Integer.parseInt(arr[1]));
-            }
-
+            answer[i] = search(arr[0], Integer.parseInt(arr[1]));
 
         }
 
         return answer;
     }
 
-    void getCombination(int depth,String input ,String[] arr){
+    void getCombination(int depth, String input, String[] arr) {
 
-        if(depth==4){
+        if (depth == 4) {
             if (!map.containsKey(input)) {
-                map.put(input, new ArrayList<>());
+                ArrayList<Integer> list = new ArrayList<>(Collections.singletonList(Integer.parseInt(arr[4])));
+                map.put(input, list);
+            } else {
+                map.get(input).add(Integer.parseInt(arr[4]));
             }
-            map.get(input).add(Integer.parseInt(arr[4]));
-            return ;
+            return;
         }
-        getCombination(depth+1,input+"-",arr);
-        getCombination(depth+1,input + arr[depth],arr);
+        getCombination(depth + 1, input + "-", arr);
+        getCombination(depth + 1, input + arr[depth], arr);
     }
-    static int search(String key, int score){
+
+    static int search(String key, int score) {
         if(!map.containsKey(key))return 0;
         ArrayList<Integer> list = map.get(key);
 
         int start = 0;
-        int end = list.size()-1;
-        while(start <= end){
+        int end = list.size() - 1;
+        while (start <= end) {
 
-            int mid = (start + end)/2;
+            int mid = (start + end) / 2;
 
-            if(score > list.get(mid)) start = mid + 1;
+            if (score > list.get(mid)) start = mid + 1;
             else end = mid - 1;
         }
         return list.size() - start;
@@ -60,8 +62,8 @@ public class ScoreSearch {
     }
 
     public static void main(String[] args) {
-        String[] a = new String[]{"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
-        String[] b = new String[]{"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
+        String[] a = new String[]{"java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"};
+        String[] b = new String[]{"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"};
 
 
     }
