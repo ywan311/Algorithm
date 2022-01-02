b1442879269799d48f01b1510e97b640ddb4e78a
yoowankim <ywan311@naver.com>
2022-01-02 13:25:38 +0900

    2022-01-02

--- src/net/Programmers/MaxExpression.java
+++ src/net/Programmers/MaxExpression.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class MaxExpression {
     static List<Long> staticNumberList = new ArrayList<>();
     static List<String> opertators;
+    static char[][] combArr= new char[][]{{'+','-','*'},{'+','*','-'},{'*','-','+'},{'*','+','-'},{'-','+','*'},{'-','*','+'}};
     static long answer = 0L;
 
     public long solution(String expression) {
@@ -17,17 +18,46 @@ public class MaxExpression {
         opertators = new ArrayList<>(Arrays.asList(operArr));
         opertators = opertators.stream().filter(a -> !a.equals("")).collect(Collectors.toList());
 
-        Set<String> set = new HashSet<>(opertators);
 
-        System.out.println(staticNumberList);
-        System.out.println(opertators);
-
+        for (char[] chars : combArr) {
+            getMax(chars);
+        }
 
         return answer;
     }
 
+    void getMax(char[] arr){
+        List<Long> numberCal = new ArrayList<>(staticNumberList);
+
+        List<String> operatorCal = new ArrayList<>(opertators);
+        for(char c : arr){
+            for(int i =0;i<operatorCal.size();i++){
+                long tmp = 0L;
+                if(operatorCal.get(i).charAt(0)==c){
+                    switch (c){
+                        case '+':
+                            tmp = numberCal.get(i)+numberCal.get(i+1);
+                            break;
+                        case '-':
+                            tmp = numberCal.get(i)-numberCal.get(i+1);
+                            break;
+                        case '*':
+                            tmp = numberCal.get(i)*numberCal.get(i+1);
+                            break;
+                    }
+                    numberCal.remove(i);
+                    numberCal.remove(i);
+                    numberCal.add(i,tmp);
+                    operatorCal.remove(i);
+                    i--;
+                }
+            }
+        }
+        answer = Math.max(answer,Math.abs(numberCal.get(0)));
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(new MaxExpression().solution("50*6-3*2"));
+        System.out.println(new MaxExpression().solution("100-200*300-500+20"));
     }
 }
