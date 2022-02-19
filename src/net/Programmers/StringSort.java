bf2296fda2b7bd163bba5b928c37d5fa7e336f08
kimyoowan <44660911+ywan311@users.noreply.github.com>
2022-02-19 16:52:12 +0900

    2022-02-19

--- src/net/Programmers/StringSort.java
+++ src/net/Programmers/StringSort.java
@@ -1,9 +1,20 @@
 package net.Programmers;
 
+import java.util.Arrays;
+import java.util.Comparator;
 
 class StringSort {
+    
     public String[] solution(String[] strings, int n) {
-        String[] answer = {};
-        return answer;
+        Arrays.sort(strings, new Comparator<String>(){
+             @Override
+             public int compare(String o1, String o2) {
+                 if(o1.charAt(n)==o2.charAt(n)) return o1.compareTo(o2);
+                 return o1.charAt(n)-o2.charAt(n);
+             }
+         });
+        return strings;
     }
 }
+
+
