1b38ed741dbc33b4c9b00a64a41c6e798d77b3a1
ywan311 <ywan311@naver.com>
2022-05-25 18:14:44 +0900

    2022-05-25

--- src/net/Programmers/EditTable.java
+++ src/net/Programmers/EditTable.java
@@ -1,20 +1,19 @@
 package net.Programmers;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
-//프로그래머스 표편집
+/**
+* 프로그래머스 표편집
+https://programmers.co.kr/learn/courses/30/lessons/81303
+list 로 실행시 시간초과(list 안쓰는것이 핵심)
+인덱스만 가지고 있다가 마지막에 string 만들때 사용
+* */
+
 public class EditTable {
-    static List<String> list = new ArrayList<>();
-    static Stack<String> stack = new Stack<>();
-    static Stack<Integer> indexStack = new Stack<>();
+    static Stack<Integer> stack = new Stack<>();
     public String solution(int n, int k, String[] cmd) {
-        StringBuilder answer = new StringBuilder();
-        for(int i =0;i<n;i++){
-            list.add(String.valueOf(i));
-        }
         int current = k;
+        int tableSize = n;
 
         for(String command : cmd){
             if(command.startsWith("D")){
@@ -22,30 +21,31 @@ public class EditTable {
             }else if(command.startsWith("U")){
                 current-=Integer.parseInt(command.split(" ")[1]);
             }else if(command.equals("C")){
-                String s = list.get(current);
-                stack.add(s);
-                indexStack.add(current);
-                list.remove(s);
-                if(current>=list.size()){
+                stack.add(current);
+                tableSize--;
+                if(current==tableSize){
                     current--;
                 }
             }else{
-                String s = stack.pop();
-                int index = indexStack.pop();
-                list.add(index,s);
-                if(index<current)current++;
+                int index = stack.pop();
+                tableSize++;
+                if(index<=current)current++;
             }
         }
-        int index=0;
-        for(int i =0;i<n;i++){
-            if(Integer.parseInt(list.get(index))==i){
-                answer.append("O");
-                index++;
-            }else{
-                answer.append("X");
-            }
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("O".repeat(Math.max(0, tableSize)));
+        while(!stack.isEmpty()){
+            builder.insert(stack.pop(),"X");
         }
 
-        return answer.toString();
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        int k = 2;
+        String[] cmd = new String[]{"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
+        System.out.println(new EditTable().solution(n,k,cmd));
     }
 }
